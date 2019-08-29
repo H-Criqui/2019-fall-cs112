002579a0007a47603bf456cb9818b0d291d9a519
H-Criqui <hec26@humboldt.edu>
2019-08-29 14:06:54 -0700

    In class work on Remove Adjacent

--- lectures/2019-08-27 introduction/Lecture/Lecture/main.cpp
+++ lectures/2019-08-27 introduction/Lecture/Lecture/main.cpp
@@ -7,18 +7,36 @@ using namespace std;
 string removeAdjacentDuplicates(string original)
 {
 	string current;
-	string next;
+	string previous;
 	string result;
-	for (int i = 0; i < original.length() - 1; i++)
+	int index = 0;
+	while (index < original.length())
 	{
-		int space_index = original.find(' ', i);
+		int space_index = original.find(' ', index);
 		//string temp = original.substr(0, i);
+		string current = original.substr(index, space_index - index);
+
+		if(current != previous)
+		{
+			result += current + " ";
+		}
+
+		if(space_index == -1)
+		{
+			index = original.length();
+		}
+		else
+		{
+			index = space_index + 1;
+		}
+		
+		previous = current;
 		cout << space_index << endl;
 	}
-	return result;
+	return result.substr(0, result.length() - 1);
 }
 
 int main(void)
 {
-	removeAdjacentDuplicates("I I went to the the store this morning");
+	string reduced = removeAdjacentDuplicates("I I went to the the store this morning");
 }
